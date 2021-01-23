bbee366e0afae439bda8e6285b429f939a6ce034
Jiri Hoffmann <hoffmaji@mail.gvsu.edu>
2021-01-23 21:42:11 +0100

    useCascadeOnImage iOS implementation

--- ios/RNOpencv3.mm
+++ ios/RNOpencv3.mm
@@ -6,6 +6,8 @@
 #import "CvInvoke.h"
 #import "CvCamera.h"
 #import "RNOpencv3.h"
+#import <React/RCTLog.h>
+
 
 @implementation RNOpencv3
 
@@ -19,6 +21,61 @@ RCT_EXPORT_MODULE()
 {
     return @[@"onPayload"];
 }
+
+-(NSString*) getPartJSON:(cv::Mat&)dFace partKey:(NSString*)partKey part:(cv::Rect)part  {
+    
+    UIDeviceOrientation deviceOrientation = [[UIDevice currentDevice] orientation];
+    NSString* sb = @"";
+    
+    if (partKey && ![partKey isEqualToString:@""]) {
+        NSString *partKeyStr = [NSString stringWithFormat:@",\"%@\":", partKey];
+        sb = [sb stringByAppendingString:partKeyStr];
+    }
+    
+    double widthToUse = dFace.cols;
+    double heightToUse = dFace.rows;
+    
+    double X0 = part.tl().x;
+    double Y0 = part.tl().y;
+    double X1 = part.br().x;
+    double Y1 = part.br().y;
+    
+    double x = X0/widthToUse;
+    double y = Y0/heightToUse;
+    double w = (X1 - X0)/widthToUse;
+    double h = (Y1 - Y0)/heightToUse;
+    
+    switch(deviceOrientation) {
+        case UIDeviceOrientationLandscapeLeft:
+            x = 1.0 - Y1/widthToUse;
+            y = X0/heightToUse;
+            w = (Y1 - Y0)/widthToUse;
+            h = (X1 - X0)/heightToUse;
+            break;
+        case UIDeviceOrientationPortraitUpsideDown:
+            x = 1.0 - X1/widthToUse;
+            y = 1.0 - Y1/heightToUse;
+            break;
+        case UIDeviceOrientationLandscapeRight:
+            x = Y0/widthToUse;
+            y = 1.0 - X1/heightToUse;
+            w = (Y1 - Y0)/widthToUse;
+            h = (X1 - X0)/heightToUse;
+            break;
+        default:
+        case UIDeviceOrientationPortrait:
+            break;
+    }
+    
+    NSString *partStr = [NSString stringWithFormat:@"{\"x\":%f,\"y\":%f,\"width\":%f,\"height\":%f", x, y, w, h];
+    sb = [sb stringByAppendingString:partStr];
+    
+    if (partKey && ![partKey isEqualToString:@""]) {
+        sb = [sb stringByAppendingString:@"}"];
+    }
+    return sb;
+}
+
 
 RCT_EXPORT_METHOD(imageToMat:(NSString*)inPath resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
     [FileUtils imageToMat:inPath resolver:resolve rejecter:reject];
@@ -39,18 +96,18 @@ RCT_EXPORT_METHOD(matToImage:(NSDictionary*)src outPath:(NSString*)outPath resol
 }
 
 RCT_EXPORT_METHOD(cvtColor:(NSDictionary*)src dstMat:(NSDictionary*)dst convColorCode:(int)convColorCode) {
-
+    
     NSNumber *srcMatNum = [src valueForKey:@"matIndex"];
     NSNumber *dstMatNum = [dst valueForKey:@"matIndex"];
-
+    
     int srcMatIndex = (int)[srcMatNum integerValue];
     int dstMatIndex = (int)[dstMatNum integerValue];
-
+    
     Mat srcMat = [MatManager.sharedMgr matAtIndex:srcMatIndex];
     Mat dstMat = [MatManager.sharedMgr matAtIndex:dstMatIndex];
-
+    
     cvtColor(srcMat, dstMat, convColorCode);
-
+    
     [MatManager.sharedMgr setMat:dstMatIndex matToSet:dstMat];
 }
 
@@ -65,8 +122,8 @@ RCT_EXPORT_METHOD(invokeMethods:(NSDictionary*)cvInvokeMap) {
 
 // IMPT NOTE: retArr can either be one single array or an array of arrays ...
 -(void)sendCallbackData:(NSArray*)retArr callback:(NSString*)callback dstMatIndex:(int)dstMatIndex {
-    if (callback != nil && callback != (NSString*)NSNull.null && ![callback isEqualToString:@""] 
-			&& dstMatIndex >= 0 && dstMatIndex < 1000) {
+    if (callback != nil && callback != (NSString*)NSNull.null && ![callback isEqualToString:@""]
+        && dstMatIndex >= 0 && dstMatIndex < 1000) {
         if (self && retArr && retArr.count > 0) {
             [self sendEventWithName:@"onPayload" body:@{ @"payload" : retArr }];
         }
@@ -167,5 +224,63 @@ RCT_EXPORT_METHOD(deleteMat:(NSDictionary*)mat) {
 RCT_EXPORT_METHOD(deleteMats) {
     [MatManager.sharedMgr deleteMats];
 }
+
+
+RCT_EXPORT_METHOD(useCascadeOnImage:(NSString *)cascadeClassifier src:(NSDictionary*)src resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+{
+    RCTLogInfo(@"Pretending to create an event %@", cascadeClassifier);
+    
+    NSString *cascadePath = [FileUtils loadBundleResource:cascadeClassifier extension:@"xml"];
+    RCTLogInfo(@"cascade path %@", cascadePath);
+
+    CascadeClassifier cascade;
+    if (cascadePath) {
+        RCTLogInfo(@"if cascade path");
+
+        if (!cascade.load( std::string([cascadePath UTF8String]))) {
+            NSLog(@"Unable to load cascade classifier at: %@", cascadePath);
+        }
+        else {
+            RCTLogInfo(@"else - cascade loaded");
+
+            NSNumber *srcMatNum = [src valueForKey:@"matIndex"];
+            
+            int srcMatIndex = (int)[srcMatNum integerValue];
+            Mat in = [MatManager.sharedMgr matAtIndex:srcMatIndex];
+            
+            std::vector<cv::Rect> objects;
+            
+            cascade.detectMultiScale(in, objects);
+            
+            NSString *payloadJSON = @"";
+            if (objects.size() > 0) {
+                payloadJSON = [payloadJSON stringByAppendingString:@"{\"objects\":["];
+                for(size_t i = 0; i < objects.size(); i++) {
+                    
+                    NSString *faceJSON = [self getPartJSON:in partKey:@"" part:objects[i]];
+                                          payloadJSON = [payloadJSON stringByAppendingString:faceJSON];
+                                          
+                                          NSString *faceIdStr = [NSString stringWithFormat:@",\"id\":\"%d\"", (int)i];
+                                          payloadJSON = [payloadJSON stringByAppendingString:faceIdStr];
+                                          
+                                          if (i != (objects.size() - 1)) {
+                        payloadJSON = [payloadJSON stringByAppendingString:@"},"];
+                    }
+                                          else {
+                        payloadJSON = [payloadJSON stringByAppendingString:@"}"];
+                    }
+                }
+                payloadJSON = [payloadJSON stringByAppendingString:@"]}"];
+                resolve(payloadJSON);
+            } else {
+                resolve(@"{\"objects\":[]}")
+            }
+        }
+    } else {
+        reject(@"Create Event error", @"Cascade file doesn't exist", nil);
+    }
+    
+}
+
 
 @end
